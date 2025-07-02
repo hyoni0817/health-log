787933b6ce0f7d005d2bc22adbd41134d9e3bc1b
hyoni0817 <hyoni0817@naver.com>
2025-07-02 11:21:43 +0900

    Feat: Card 컴포넌트에 className prop 추가

--- src/shared/ui/components/Card.tsx
+++ src/shared/ui/components/Card.tsx
@@ -4,8 +4,9 @@ import { ReactNode } from 'react';
 
 interface CardProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const Card = ({ children }: CardProps) => {
-  return <div className="rounded-lg p-4 border-1 border-(--divider)">{children}</div>;
+export const Card = ({ children, className }: CardProps) => {
+  return <div className={`rounded-lg p-4 border-1 border-(--divider) ${className}`}>{children}</div>;
 };
