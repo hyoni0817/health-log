2af8035ac009d10abea9f9dbd3f799888772af5a
hyoni0817 <hyoni0817@naver.com>
2025-07-31 20:11:48 +0900

    Feat: 폼 필드 스타일 추가
    - 폼 필드에서 공통적으로 사용할 FormFieldStyles를 정의하여 폼 필드의 레이블, 입력 필드, 입력 래퍼 스타일을 설정함.

---
+++ src/shared/ui/FormFields/styles.ts
@@ -0,0 +1,6 @@
+export const FormFieldStyles = {
+  label: 'text-sm text-(--text) mb-1',
+  formField: 'flex flex-col gap-1 mb-5',
+  inputWrapper: 'border border-(--divider) rounded-md p-2',
+  input: 'outline-none text-(--text)',
+} as const;
