41898b709459a7d96df7cfd60a68d1fd85967106
hyoni0817 <hyoni0817@naver.com>
2025-07-31 11:18:30 +0900

    Feat: 혈당 데이터 추가 버튼 클릭 시 모달이 열리는 기능 구현
    - QuickActionsSection 컴포넌트에 혈당 데이터 추가 버튼 클릭 시 AddBloodSugarDataModal이 열리는 기능을 추가함.
    
    - useModal 훅을 사용하여 모달을 관리하도록 설정함.

--- src/widgets/home/ui/QuickActionsSection.tsx
+++ src/widgets/home/ui/QuickActionsSection.tsx
@@ -1,14 +1,30 @@
+'use client';
+
 import React from 'react';
 import { Button, Card } from '@/shared/ui';
 import { Droplet, HeartPulse, Plus } from 'lucide-react';
+import { useModal } from '@/shared/ui';
+import AddBloodSugarDataModal from '@/features/blood-sugar/ui/AddBloodSugarDataModal';
 
 export const QuickActionsSection = () => {
+  const { openModal } = useModal();
+
+  const handleAddBloodSugarData = () => {
+    openModal(AddBloodSugarDataModal);
+  };
+
   return (
     <section className="mb-8 w-[40%] flex-1">
       <Card className="h-full">
         <h2 className="text-white text-xl font-semibold mb-4">빠른 실행</h2>
         <div className="space-y-3">
-          <Button variant="action" fullWidth leftIcon={<Droplet />} rightIcon={<Plus />}>
+          <Button
+            onClick={handleAddBloodSugarData}
+            variant="action"
+            fullWidth
+            leftIcon={<Droplet />}
+            rightIcon={<Plus />}
+          >
             혈당 데이터 추가
           </Button>
           <Button variant="action" fullWidth leftIcon={<HeartPulse />} rightIcon={<Plus />}>
