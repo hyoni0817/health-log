7491902324cae901b4d0edd4642798e98fb0d065
hyoni0817 <hyoni0817@naver.com>
2025-08-12 20:26:53 +0900

    Feat: 건강 상태 메시지를 표현하는 공통 컴포넌트 구현
    - 건강 상태를 시각적으로 표시하는 HealthMessage 컴포넌트를 새로 구현함.
    
    - 상태에 따라 아이콘과 배경 색상을 다르게 설정하여 사용자에게 직관적인 정보를 제공함.
    
    - 컴포넌트는 수치, 상태, 라벨, 단위 및 추천 사항을 포함함.

---
+++ src/shared/ui/HealthMessage/ui.tsx
@@ -0,0 +1,60 @@
+import { CircleCheckBigIcon, OctagonAlertIcon, TriangleAlertIcon, CircleX } from 'lucide-react';
+import React from 'react';
+import { CommonStatusKey } from '@/shared/types/status';
+
+interface HealthMessageProps {
+  value: string;
+  status: CommonStatusKey;
+  label: string;
+  unit: string;
+  recommendation: string;
+}
+
+export const HealthMessage = ({ value, status, unit, label, recommendation }: HealthMessageProps) => {
+  const iconSize = 'h-5 w-5';
+  const icon = {
+    LOW: <TriangleAlertIcon className={iconSize} />,
+    NORMAL: <CircleCheckBigIcon className={iconSize} />,
+    BORDERLINE: <OctagonAlertIcon className={iconSize} />,
+    HIGH_RISK: <OctagonAlertIcon className={iconSize} />,
+    RECHECK: <CircleX className={iconSize} />,
+  };
+
+  const statusClasses = {
+    LOW: {
+      background: 'bg-(--color-yellow-50)',
+      titleText: 'text-(--color-orange-400)',
+    },
+    NORMAL: {
+      background: 'bg-(--color-green-50)',
+      titleText: 'text-(--color-green-400)',
+    },
+    BORDERLINE: {
+      background: 'bg-(--color-orange-50)',
+      titleText: 'text-(--color-orange-600)',
+    },
+    HIGH_RISK: {
+      background: 'bg-(--color-red-50)',
+      titleText: 'text-(--color-red-500)',
+    },
+    RECHECK: {
+      background: 'bg-(--color-gray-50)',
+      titleText: 'text-(--color-blue-400)',
+    },
+  };
+
+  return (
+    <div className={`${statusClasses[status].background} p-5 rounded-md mb-4`}>
+      <div className="flex items-center gap-3">
+        <div className={`${statusClasses[status].titleText}`}>{icon[status]}</div>
+        <div>
+          <p className={`text-base font-bold mb-1 ${statusClasses[status].titleText}`}>현재 혈당 상태: {label}</p>
+          <p className="text-sm text-(--color-gray-500) mb-1">
+            {value} {unit}
+          </p>
+          <p className="text-sm text-(--color-gray-500)">* {recommendation}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
