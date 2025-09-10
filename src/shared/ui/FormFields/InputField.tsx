099734eda4e95526929a39c3ad2da3267080688f
hyoni0817 <hyoni0817@naver.com>
2025-09-11 02:11:52 +0900

    Feat: input 태그 기반 입력 필드 컴포넌트 추가
    - 숫자만 입력할 수 있도록 하는 'number'라는 FieldType을 제공함과 동시에 'number'와 함께 사용하면 안되는 속성을 조건부 타입을 통해 제어함.
    
    - 값을 입력하지 않은 경우 에러 메시지가 표시되게 함.

---
+++ src/shared/ui/FormFields/InputField.tsx
@@ -0,0 +1,66 @@
+import React, { InputHTMLAttributes } from 'react';
+import { filterNonNumericInput } from '@/shared/utils';
+
+type FieldType = 'number' | 'text';
+
+// 조건부 타입: fieldType이 'number'면 fieldTypeAttributes에서 지정한 pattern, inputMode 값이 있기 때문에 pattern, inputMode 사용을 금지함.
+type ConditionalInputProps<T extends FieldType> = T extends 'number'
+  ? {
+      fieldType: T;
+      pattern?: never; // 사용 금지
+      inputMode?: never; // 사용 금지
+      onInput?: never;
+    }
+  : {
+      fieldType: T;
+      pattern?: string;
+      inputMode?: InputHTMLAttributes<HTMLInputElement>['inputMode'];
+      onInput?: InputHTMLAttributes<HTMLInputElement>['onInput'];
+    };
+
+type InputFieldProps<T extends FieldType = FieldType> = Omit<
+  InputHTMLAttributes<HTMLInputElement>,
+  'pattern' | 'inputMode'
+> &
+  ConditionalInputProps<T> & {
+    error?: string;
+  };
+
+const InputField = <T extends FieldType = 'text'>(props: InputFieldProps<T>) => {
+  const { id, value, onChange, fieldType = 'text', placeholder, error, ...rest } = props;
+
+  const fieldTypeAttributes: Record<FieldType, InputHTMLAttributes<HTMLInputElement>> = {
+    number: {
+      pattern: '[0-9]*',
+      inputMode: 'numeric',
+      onInput: filterNonNumericInput,
+    },
+    text: {},
+  };
+
+  return (
+    <>
+      <div className={Styles.inputWrapper}>
+        <input
+          type="text"
+          id={id}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+          className={Styles.input}
+          {...rest}
+          {...(fieldTypeAttributes && fieldTypeAttributes[fieldType])}
+        />
+      </div>
+
+      {error && <p className="text-sm text-(--color-red-500) mt-1">{error}</p>}
+    </>
+  );
+};
+
+export default InputField;
+
+const Styles = {
+  inputWrapper: 'border border-(--divider) rounded-md p-2',
+  input: 'outline-none text-(--text)',
+};
