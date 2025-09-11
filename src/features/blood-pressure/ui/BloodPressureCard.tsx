7fbc2cd8949544068c80a224153e4ab7bfda4219
hyoni0817 <hyoni0817@naver.com>
2025-09-11 16:30:17 +0900

    Feat: 혈압 카드 컴포넌트를 서버 컴포넌트로 변경 및 최근 혈압 기록 불러오기 api 연동
    - 서버 컴포넌트로 변경됨에 따라 배럴 파일에 추가
    
    - '-일전'등으로 글자를 표현하기 위해 dayjs의 fromNow를 사용함.
    
    - 혈압 상태에 따른 텍스트 색상도 적용함.
    
    - 혈압 기록이 없을 경우, 항목을 모두 숨김 처리하고 혈압 기록이 없다는 메시지를 표시하도록 개선함.

--- src/features/blood-pressure/ui/BloodPressureCard.tsx
+++ src/features/blood-pressure/ui/BloodPressureCard.tsx
@@ -1,30 +1,56 @@
-'use client';
-
-import { Card } from '@/shared/ui/Card';
 import React from 'react';
+import { Card } from '@/shared/ui/Card';
+import { bloodPressureApi } from '@/entities/blood-pressure/model/api/bloodPressureApi';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import { getCommonStatusTextColor } from '@/shared/utils/status';
+import { getBloodPressureStatusLabel } from '../lib/status';
+import { getBloodPressureStatus } from '../lib/status';
 
-export const BloodPressureCard = () => {
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
+export const BloodPressureCard = async () => {
+  const bloodPressureLatest = await bloodPressureApi.getLatestBloodPressure();
+
+  const bloodPressureStatus = getBloodPressureStatus(
+    Number(bloodPressureLatest?.systolic_bp),
+    Number(bloodPressureLatest?.diastolic_bp)
+  );
+
   return (
     <Card className="w-1/2">
       <div className="flex items-center justify-between gap-2">
         <h3 className="text-base font-medium text-(--text) mb-2">최근 혈압 수치</h3>
-        <p className="text-sm text-(--text-subtitle)">마지막 업데이트: 3일전</p>
+        {bloodPressureLatest?.date && (
+          <p className="text-sm text-(--text-subtitle)">
+            마지막 업데이트: {dayjs(bloodPressureLatest?.date).fromNow()}
+          </p>
+        )}
       </div>
 
-      <div className="pb-3">
-        <div className="flex items-center gap-3">
-          <div className="flex items-center gap-1">
-            <span className="text-2xl font-bold text-(--text)">100/80</span>
-            <span className="text-base text-(--text-subtitle)">mmHg</span>
+      {bloodPressureLatest ? (
+        <div className="pb-3">
+          <div className="flex items-center gap-3">
+            <div className="flex items-center gap-1">
+              <span className="text-2xl font-bold text-(--text)">
+                {bloodPressureLatest?.systolic_bp}/{bloodPressureLatest?.diastolic_bp}
+              </span>
+              <span className="text-base text-(--text-subtitle)">mmHg</span>
+            </div>
+
+            <div className={`text-sm ${getCommonStatusTextColor(bloodPressureStatus)}`}>
+              {getBloodPressureStatusLabel(bloodPressureStatus)}
+            </div>
           </div>
 
-          <div className="text-sm text-(--color-green-500)">양호</div>
+          <div>
+            <p className="text-sm text-(--text-subtitle)">맥박: {bloodPressureLatest?.heart_rate} bpm</p>
+          </div>
         </div>
-
-        <div>
-          <p className="text-sm text-(--text-subtitle)">Pulse: 70 bpm</p>
-        </div>
-      </div>
+      ) : (
+        <div className="text-sm text-(--text-subtitle)">혈압 기록이 없습니다.</div>
+      )}
     </Card>
   );
 };
--- src/features/blood-sugar/index.ts
+++ src/features/blood-sugar/index.ts
@@ -1,4 +1,5 @@
 // 서버 사이드만 추가
+export { BloodSugarCard } from './ui/BloodSugarCard';
 
 // hooks
 export { useCreateBloodSugar } from './hooks/useCreateBloodSugar';
