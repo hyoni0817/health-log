6046cc704e1b7c4660da266e515dc37be3b03027
hyoni0817 <hyoni0817@naver.com>
2025-09-11 16:31:13 +0900

    Feat: 혈당 카드 컴포넌트를 서버 컴포넌트로 변경 및 최근 혈당 기록 불러오기 api 연동
    - 서버 컴포넌트로 변경됨에 따라 배럴 파일에 추가
    
    - '-일전'등으로 글자를 표현하기 위해 dayjs의 fromNow를 사용함.
    
    - 혈당 상태에 따른 텍스트 색상도 적용함.
    
    - 혈당 기록이 없을 경우, 항목을 모두 숨김 처리하고 혈당 기록이 없다는 메시지를 표시하도록 개선함.

--- src/features/blood-pressure/index.ts
+++ src/features/blood-pressure/index.ts
@@ -1,4 +1,7 @@
 // 서버 사이드만 추가
 
+// ui
+export { BloodPressureCard } from './ui/BloodPressureCard';
+
 // hooks
 export { useCreateBloodPressure } from './hooks/useCreateBloodPressure';
--- src/features/blood-sugar/ui/BloodSugarCard.tsx
+++ src/features/blood-sugar/ui/BloodSugarCard.tsx
@@ -1,23 +1,48 @@
-'use client';
-
-import { Card } from '@/shared/ui/Card';
 import React from 'react';
+import { Card } from '@/shared/ui/Card';
+import dayjs from 'dayjs';
+import { getBloodSugarStatus, getBloodSugarStatusLabel } from '../lib/status';
+import { MeasurementTiming, PostMealTime } from '@/shared/types/measurement';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
+import { getCommonStatusTextColor } from '@/shared/utils/status';
+import { bloodSugarApi } from '@/entities/blood-sugar/model/api/bloodSugarApi';
 
-export const BloodSugarCard = () => {
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
+export const BloodSugarCard = async () => {
+  const bloodSugarLatest = await bloodSugarApi.getLatestBloodSugar();
+
+  const bloodSugarStatus = getBloodSugarStatus(
+    Number(bloodSugarLatest?.value),
+    bloodSugarLatest?.measurement_timing as MeasurementTiming,
+    bloodSugarLatest?.post_meal_time as PostMealTime
+  );
+
   return (
     <Card className="w-1/2">
       <div className="flex items-center justify-between gap-2">
         <h3 className="text-base font-medium text-(--text) mb-2">최근 혈당 수치</h3>
-        <div className="text-sm text-(--text-subtitle)">마지막 업데이트: 3일전</div>
+        {bloodSugarLatest?.date && (
+          <p className="text-sm text-(--text-subtitle)">마지막 업데이트: {dayjs(bloodSugarLatest?.date).fromNow()}</p>
+        )}
       </div>
-      <div className="flex items-center gap-3 pb-3">
-        <div className="flex items-center gap-1">
-          <span className="text-2xl font-bold text-(--text)">100</span>
-          <span className="text-base text-(--text-subtitle)">mg/dL</span>
-        </div>
 
-        <div className="text-sm text-(--color-green-500)">양호</div>
-      </div>
+      {bloodSugarLatest ? (
+        <div className="flex items-center gap-3 pb-3">
+          <div className="flex items-center gap-1">
+            <span className="text-2xl font-bold text-(--text)">{bloodSugarLatest?.value}</span>
+            <span className="text-base text-(--text-subtitle)">mg/dL</span>
+          </div>
+
+          <div className={`text-sm ${getCommonStatusTextColor(bloodSugarStatus)}`}>
+            {getBloodSugarStatusLabel(bloodSugarStatus)}
+          </div>
+        </div>
+      ) : (
+        <div className="text-sm text-(--text-subtitle)">혈당 기록이 없습니다.</div>
+      )}
     </Card>
   );
 };
