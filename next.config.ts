6265849aaf82e63b6d35227ff8779ce103947dcb
hyoni0817 <hyoni0817@naver.com>
2025-11-03 06:42:20 +0900

    Hotfix: 빌드시 ESLint, 타입 체크 비활성화
    - ec2 인스턴스에서 빌드 할 때 너무 느려서 비활성화 해둠.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // TypeScript 타입 체크 비활성화
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // ESLint 검사 비활성화
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
