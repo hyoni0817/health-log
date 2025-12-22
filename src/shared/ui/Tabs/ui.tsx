19f852f5bd25be3ab029c1b2623d688d6779c8c0
hyoni0817 <hyoni0817@naver.com>
2025-12-22 18:12:07 +0900

    Feat: 공통 Tabs 컴포넌트 구현

---
+++ src/shared/ui/Tabs/ui.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from 'react';
+
+interface TabsProps {
+  items: {
+    key: string | number;
+    label: string;
+    children: React.ReactNode;
+  }[];
+}
+
+export const Tabs: FC<TabsProps> = (props) => {
+  const { items } = props;
+  const [activeTab, setActiveTab] = useState(items[0].key);
+
+  const handleClickTab = (key: TabsProps['items'][number]['key']) => {
+    setActiveTab(key);
+  };
+
+  return (
+    <div>
+      <div className="flex gap-2 mb-4">
+        {items.map((item) => (
+          <button
+            key={item.key}
+            onClick={() => handleClickTab(item.key)}
+            className={`px-3 py-2 shadow-[inset_0_0_0_1px_var(--divider)] rounded-md text-sm ${activeTab === item.key ? 'bg-(--color-white) text-(--color-gray-900)' : 'text-(--color-white)'}`}
+          >
+            {item.label}
+          </button>
+        ))}
+      </div>
+
+      <div>{items.find((item) => item.key === activeTab)?.children}</div>
+    </div>
+  );
+};
