4abfc420e29c2819d8dea5e1fc4a059e69c1bb52
hyoni0817 <hyoni0817@naver.com>
2026-01-31 17:26:32 +0900

    Style: ExportButton 패딩 및 글자 사이즈 변경

--- src/shared/ui/ExportButton/ui.tsx
+++ src/shared/ui/ExportButton/ui.tsx
@@ -10,10 +10,10 @@ export const ExportButton: FC<ExportButtonProps> = (props) => {
   return (
     <button
       {...rest}
-      className="bg-(--color-white) text-(--color-gray-900) rounded-2xl p-2 flex items-center gap-2 px-3 py-3"
+      className="bg-(--color-white) text-(--color-gray-900) rounded-2xl p-2 flex items-center gap-2 px-3 py-2"
     >
       <DownloadIcon className="text-(--color-gray-900) font-black" />
-      <span className="text-md font-black">{text}</span>
+      <span className="text-sm font-black">{text}</span>
     </button>
   );
 };
