bfd1aec4574b7c351b1ea76c806425deac5253c2
hyoni0817 <hyoni0817@naver.com>
2026-02-25 00:42:38 +0900

    Feat: 공통 Tabs 컴포넌트에서 옵셔널 하고 탭 왼쪽 영역에 컨텐츠를 표현할 수 있도록 구현

--- src/shared/ui/Tabs/ui.tsx
+++ src/shared/ui/Tabs/ui.tsx
@@ -7,10 +7,11 @@ interface TabsProps {
     children: React.ReactNode;
   }[];
   onChange: (key: string | number) => void;
+  tabLeftContent?: React.ReactNode;
 }
 
 export const Tabs: FC<TabsProps> = (props) => {
-  const { items, onChange } = props;
+  const { items, onChange, tabLeftContent } = props;
   const [activeTab, setActiveTab] = useState(items[0].key);
 
   const handleClickTab = (key: TabsProps['items'][number]['key']) => {
@@ -20,16 +21,19 @@ export const Tabs: FC<TabsProps> = (props) => {
 
   return (
     <div>
-      <div className="flex gap-2 mb-4">
-        {items.map((item) => (
-          <button
-            key={item.key}
-            onClick={() => handleClickTab(item.key)}
-            className={`px-3 py-2 shadow-[inset_0_0_0_1px_var(--divider)] rounded-md text-sm ${activeTab === item.key ? 'bg-(--color-white) text-(--color-gray-900)' : 'text-(--color-white)'}`}
-          >
-            {item.label}
-          </button>
-        ))}
+      <div className="flex items-center justify-between">
+        <div className="flex gap-2 mb-4">
+          {items.map((item) => (
+            <button
+              key={item.key}
+              onClick={() => handleClickTab(item.key)}
+              className={`px-3 py-2 shadow-[inset_0_0_0_1px_var(--divider)] rounded-md text-sm ${activeTab === item.key ? 'bg-(--color-white) text-(--color-gray-900)' : 'text-(--color-white)'}`}
+            >
+              {item.label}
+            </button>
+          ))}
+        </div>
+        {tabLeftContent && <div>{tabLeftContent}</div>}
       </div>
 
       <div>{items.find((item) => item.key === activeTab)?.children}</div>
