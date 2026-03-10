010f3b0a1beab50a217b9af333e9203131a48d45
hyoni0817 <hyoni0817@naver.com>
2026-03-10 17:56:03 +0900

    Refactor: 혈당 추이 조회 hook에 options 매개변수 추가 및 jsDoc 수정
    - pdf 내보내기 상태에 따라 allHistory 혹은 history 쿼리 중 하나만 실행시켜야 해서 option 중 enalbed를 사용해야 했음. 그래서 options 매개변수를 추가해줌.

--- src/features/blood-sugar/hooks/useGetBloodSugarHistory.ts
+++ src/features/blood-sugar/hooks/useGetBloodSugarHistory.ts
@@ -1,14 +1,19 @@
-import { bloodSugarQueries } from '@/entities/blood-sugar/model';
+import { bloodSugarQueries, BloodSugarRecord } from '@/entities/blood-sugar/model';
 import { HistoryParams } from '@/shared/types/history';
+import { CustomQueryOptions } from '@/shared/types/query';
 import { useQuery } from '@tanstack/react-query';
 
 /**
  * 혈당 추이 조회 hook
- * @param days 조회할 일 수
- * @param startDate 시작 날짜
- * @param endDate 종료 날짜
+ * @param params HistoryParams (periodType, limit, offset, days, month, startDate, endDate)
+ * @param options 리액트 쿼리 옵션 (enabled, staleTime, gcTime, refetchOnWindowFocus, refetchOnMount, refetchOnReconnect)
+ * @example
+ * const { data } = useGetBloodSugarHistory({
+ *   periodType: PeriodFilterType.DAY,
+ *   days: 7,
+ * }, { enabled: true });
  * @returns 혈당 추이 데이터
  */
-export const useGetBloodSugarHistory = (params: HistoryParams) => {
-  return useQuery(bloodSugarQueries.history(params));
+export const useGetBloodSugarHistory = (params: HistoryParams, options?: CustomQueryOptions<BloodSugarRecord[]>) => {
+  return useQuery(bloodSugarQueries.history(params, options));
 };
