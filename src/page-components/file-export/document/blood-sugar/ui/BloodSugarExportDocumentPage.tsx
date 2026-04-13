50baf67becf45bc679d6ea6f11b74abb48d6e142
hyoni0817 <hyoni0817@naver.com>
2026-04-14 00:01:15 +0900

    Fix: useSearchParams()를 사용한 컴포넌트에 Suspense를 감싸지 않아서 발생한 오류 해결

--- src/page-components/file-export/document/blood-sugar/ui/BloodSugarExportDocumentPage.tsx
+++ src/page-components/file-export/document/blood-sugar/ui/BloodSugarExportDocumentPage.tsx
@@ -1,9 +1,12 @@
 import { BloodSugarAnalysisDocument } from '@/features/blood-sugar/ui/BloodSugarAnalysisDocument';
+import { Suspense } from 'react';
 
 export default function BloodSugarExportDocumentPage() {
   return (
     <main>
-      <BloodSugarAnalysisDocument />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BloodSugarAnalysisDocument />
+      </Suspense>
     </main>
   );
 }
