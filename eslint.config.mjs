2cf5691da0c72f898fe6799279e0f7b16fbc09fb
hyoni0817 <hyoni0817@naver.com>
2026-06-05 21:36:43 +0900

    Set: Cypress 구성 및 TypeScript 지원 파일 추가
    - Cypress 테스트 환경 설정을 위해 cypress.config.ts 파일을 도입함.
    
    - Cypress 테스트용 TypeScript를 구성하기 위해 cypress 폴더 내에 tsconfig.json 파일을 추가함.
    
    - const { defineConfig } = require('cypress'); 에서 require에 eslint 에러가 발생하여 @typescript-eslint/no-require-imports rules를 off 해줌.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
 
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -12,10 +12,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
   },
-}, ...storybook.configs["flat/recommended"]];
+  ...storybook.configs['flat/recommended'],
+];
 
 export default eslintConfig;
